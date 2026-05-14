e2f57bfca6b8afb942f56bcece8a6eaa05f1264d
Purohit Iyer <39787733+GeekyPython@users.noreply.github.com>
2026-05-15 02:20:27 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> checker;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(checker.find(nums[i]) != checker.end()) return true;
+            checker.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
